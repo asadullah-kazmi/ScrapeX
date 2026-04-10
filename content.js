abb418df91416ef5124394ebbc7a8fde414707a7
asadullah-kazmi <asadullahkazmi.007@gmail.com>
2026-04-10 23:51:34 +0500

    Implement function to add checkboxes to table rows

--- content.js
+++ content.js
@@ -1,3 +1,25 @@
 "use strict";
 
+function addRowCheckboxes() {
+  const rows = document.querySelectorAll("tbody tr");
+
+  rows.forEach((row) => {
+    if (row.querySelector(".scrape-checkbox")) {
+      return;
+    }
+
+    const firstCell = row.firstElementChild;
+    if (!firstCell || firstCell.tagName !== "TD") {
+      return;
+    }
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("scrape-checkbox");
+
+    firstCell.prepend(checkbox);
+  });
+}
+
+addRowCheckboxes();
 console.log("Extension loaded");
